24ac6bd17fed5eccbfc04405e87ba7892f0168f5
Sachin Kumar Gupta <sachingupta959@gmail.com>
2021-06-15 21:26:22 +0530

    calculator

--- Calculator/js.js
+++ Calculator/js.js
@@ -1,17 +1,29 @@
-var buttons = document.querySelectorAll("button");
-var result = document.querySelector('input');
+var screen = document.querySelector('input');
+var buttons = document.querySelectorAll('button');
+var result = "";
 
 buttons.forEach(button => {
-    button.addEventListener('click', e => {
-        let str = button.innerText;
-        if(str == 'C') {
-            result.value = "";
+    button.addEventListener('click', (e, err) => {
+        if(button.innerText == 'AC')
+        {
+            result = "";
+            screen.value = "";
         }
-        else if(str == '=') {
-            result.value = parseFloat(eval(result.value)).toFixed(5);
+        else if(button.innerText == '=')
+        {
+            screen.value = (eval(result)).toFixed(4);
+            result = screen.value;
         }
-        else {
-            result.value +=str;
+        else if(button.innerText == 'X')
+        {
+            result = result.slice(0,result.length-1);
+            screen.value = result;
         }
+        else
+        {
+            result+=button.innerText;
+            screen.value = result;
+        }
+        // console.log(5);
     });
 });
