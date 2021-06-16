568202c996ab0ef67b220b8bd013e468632da453
Sachin Kumar Gupta <sachingupta959@gmail.com>
2021-06-16 23:24:46 +0530

    THA8

--- THA8/js.js
+++ THA8/js.js
@@ -1,8 +1,20 @@
+var row = "<tr>";
+for(let i=0;i<20;i++) {
+    row+="<td></td>";
+}
+row+="</tr>";
+var content = "";
+
+for(let i=0;i<20;i++) {
+    content+=row;
+}
+var tab = document.querySelector('table');
+tab.innerHTML = content;
+
 var cells = document.querySelectorAll('td');
 
 cells.forEach(cell => {
     cell.addEventListener('click', e => {
-        cell.style.backgroundColor = "white";
-        cell.style.border = "none";
+        cell.classList.toggle("white");
     });
 });
