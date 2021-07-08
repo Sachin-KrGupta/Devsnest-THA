9b9a7889484db461686e59a27a33bb785dcd90be
Sachin Kumar Gupta <sachingupta959@gmail.com>
2021-07-08 19:17:41 +0530

    THA16 improved

--- tha16/src/Chess.js
+++ tha16/src/Chess.js
@@ -1,10 +1,37 @@
-import Row from './Row.js';
 import './Chess.css';
+
+function Box() {
+    return (
+        <div className="box"></div>
+    );
+}
+
+function Row() {
+    return (
+        <div className="row">
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+        </div>
+    );
+}
 
 function Chess(props) {
     return (
         <div className="board">
-            <Row />
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
+            <Row></Row>
         </div>
     );
 }
